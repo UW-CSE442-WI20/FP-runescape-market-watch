63da42cc6b1e174c9b666d5486eb6ed4c00dac43
prestonjcrowe <prestonjcrowe@gmail.com>
2020-05-30 18:12:44 -0700

    npm run build

---
+++ build/precache-manifest.e71fc34f195d5c813ffce28e63a66eb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b775f7c5a0d1c4536fb46a51864219",
+    "url": "/FP-runescape-market-watch/index.html"
+  },
+  {
+    "revision": "2ab4d791d99b7b4d7e92",
+    "url": "/FP-runescape-market-watch/static/css/main.cb0c7059.chunk.css"
+  },
+  {
+    "revision": "01835389c5f3d7a9035e",
+    "url": "/FP-runescape-market-watch/static/js/2.ad8b336b.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/FP-runescape-market-watch/static/js/2.ad8b336b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab4d791d99b7b4d7e92",
+    "url": "/FP-runescape-market-watch/static/js/main.c612c259.chunk.js"
+  },
+  {
+    "revision": "5f8b5416a0f96d894f6b",
+    "url": "/FP-runescape-market-watch/static/js/runtime-main.95b16fa2.js"
+  }
+]);
